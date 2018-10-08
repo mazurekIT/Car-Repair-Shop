5a05225c52e69387e016112124e21d46a1e342b0
Mateusz Mazurek <mazzi198913@wp.pl>
2018-10-08 23:32:04 +0200

    vehicle - saveToDb 1.1

--- src/main/java/pl/coderslab/controller/vehicle/VehicleSaveToDb.java
+++ src/main/java/pl/coderslab/controller/vehicle/VehicleSaveToDb.java
@@ -1,11 +1,15 @@
 package pl.coderslab.controller.vehicle;
 
+import pl.coderslab.dao.VehicleDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.SQLException;
 
 @WebServlet(name = "VehicleSaveToDb")
 public class VehicleSaveToDb extends HttpServlet {
@@ -16,11 +20,22 @@ public class VehicleSaveToDb extends HttpServlet {
             link = getInitParameter("link");
         } catch (Exception e) {
             System.out.println("Bląd linków");
-
         }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String brand = request.getParameter("brand");
+        String model = request.getParameter("model");
+        int production_date = Integer.parseInt(request.getParameter("production_date"));
+        String plate_number = request.getParameter("plate_number");
+        Date next_service_date = Date.valueOf(request.getParameter("next_service_date"));
+        int customer_id = Integer.parseInt(request.getParameter("customer_id"));
+        VehicleDao vehicle = new VehicleDao(brand,model,production_date,plate_number,next_service_date,customer_id);
+        try {
+            vehicle.saveToDB();
+        } catch (SQLException e) {
+            System.out.println("Błąd save");
+        }
 
     }
 
--- src/main/webapp/WEB-INF/vehicle-forms/saveToDb.jsp
+++ src/main/webapp/WEB-INF/vehicle-forms/saveToDb.jsp
@@ -1,28 +1,22 @@
-<form>
-    Model:
-    <input type="text" name="model">
-    <br>
+<form action="/" method="post">
     Marka:
     <input type="text" name="brand">
     <br>
-
+    Model:
+    <input type="text" name="model">
+    <br>
     Rok produkcji:
     <input type="number" name="production_date">
     <br>
-
     Nr rejestracyjny:
     <input type="text" name="plate_number">
     <br>
-
     Data kolejnego przeglądu:
     <input type="date" name="next_service_date">
     <br>
-
     Id klienta:
     <input type="number" name="customer_id">
     <br>
-
-
     <input type="submit" value="Wyślij">
 
 </form>
