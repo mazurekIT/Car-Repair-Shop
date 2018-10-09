11b499fd51b163d7df157466c7f8323443f983c2
kilo_mike <niewiem826@gmail.com>
2018-10-09 09:54:19 +0200

    dzialajace save to db

--- src/main/java/pl/coderslab/filters/EncodingFilter.java
+++ src/main/java/pl/coderslab/filters/EncodingFilter.java
@@ -1,24 +1,4 @@
-package pl.coderslab.filters;package pl.coderslab.filters;
-
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import java.io.IOException;
-
-@WebFilter(filterName = "EncodingFilter")
-public class EncodingFilter implements Filter {
-    public void destroy() {
-    }
-
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(req, resp);
-    }
-
-    public void init(FilterConfig config) throws ServletException {
-
-    }
-
-}
-
+package pl.coderslab.filters;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
